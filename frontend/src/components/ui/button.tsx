4920109cacb6d45f144ee168b18fdc08212fdc5f
joenash156 <joenash156@gmail.com>
2026-01-05 20:02:20 +0000

    Fixed one more error

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-eaimport * as React from "react"
+import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -56,4 +56,5 @@ function Button({
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export { Button, buttonVariants }
